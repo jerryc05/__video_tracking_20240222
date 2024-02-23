7e3a2ba61ea98b8e3ad6b8a7f43ea64abc54ce58
Jerry Chen <jerryc443@gmail.com>
2024-02-22 22:42:23 -0500

    add shadcnui

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,62 @@
-import { createSignal, type Component } from 'solid-js'
+import { createSignal } from 'solid-js'
 
 import sampleVideo from '/1.mp4'
+import { Button } from './components/ui/button'
+import { Input } from './components/ui/input'
+import { Card, CardHeader, CardTitle, CardContent } from './components/ui/card'
 
-const App: Component = () => {
-  let video: HTMLVideoElement | null = null
-  const [filepath, setFilepath] = createSignal(sampleVideo)
-  const [timestamp, setTimestamp] = createSignal(0)
+export const App = () => {
   return (
-    <>
-      <div>file path</div>
-      <input
-        type='text'
-        value={filepath()}
-        onChange={e => setFilepath(e.target.value)}
-      />
-      {/*  */}
-      <div />
-      {/* biome-ignore lint/a11y/useMediaCaption: <explanation> */}
-      <video
-        src={filepath()}
-        ref={e => {
-          video = e
-        }}
-        style='max-width:100%;max-height:100%'
-        controls
-      />
-      {/*  */}
-      <div />
-      <input
-        type='number'
-        value={timestamp()}
-        onChange={e => setTimestamp(parseFloat(e.target.value))}
-      />
-      <button
-        type='button'
-        onClick={() => {
-          if (video) {
-            video.currentTime = timestamp()
-          }
-        }}
-      >
-        Jump to video second
-      </button>
-    </>
+    <div class='mx-[5%]'>
+      <UploadConfig />
+      <ShowVideo />
+    </div>
   )
 }
 
-export default App
+const UploadConfig = () => (
+  <Card class='mx-auto my-5'>
+    <CardHeader>
+      <CardTitle>Config File Upload</CardTitle>
+      {/* <CardDescription></CardDescription> */}
+    </CardHeader>
+    <CardContent class='flex gap-x-2'>
+      <Input type='file' name='configFile' />
+      <Button type='submit'>Upload</Button>
+    </CardContent>
+  </Card>
+)
+
+const ShowVideo = () => {
+  let video: HTMLVideoElement | null = null
+  const [timestamp, setTimestamp] = createSignal(0)
+  return (
+    <div class='[&>*]:my-3'>
+      {/* biome-ignore lint/a11y/useMediaCaption: <explanation> */}
+      <video
+        src={sampleVideo}
+        ref={e => {
+          video = e
+        }}
+        controls
+      />
+      <div class='flex gap-x-2'>
+        <Input
+          type='number'
+          value={timestamp()}
+          onChange={e => setTimestamp(parseFloat(e.target.value))}
+        />
+        <Button
+          type='button'
+          onClick={() => {
+            if (video) {
+              video.currentTime = timestamp()
+            }
+          }}
+        >
+          Jump
+        </Button>
+      </div>
+    </div>
+  )
+}
